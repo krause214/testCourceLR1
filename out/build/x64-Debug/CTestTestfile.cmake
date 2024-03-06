b0f33d70c698e47a27dd7126df25480fa01ff757
Andrey Babincev <andron-2102@mail.ru>
2024-03-07 00:49:19 +0400

    Добавление файлов из папки out

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: D:/repositories/visualStudio/testProjectRepository/LR1
+# Build directory: D:/repositories/visualStudio/testProjectRepository/LR1/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("bin")
+subdirs("tests")
---
+++ out/build/x64-Debug/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/repositories/visualStudio/testProjectRepository/LR1/tests
+# Build directory: D:/repositories/visualStudio/testProjectRepository/LR1/out/build/x64-Debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("D:/repositories/visualStudio/testProjectRepository/LR1/out/build/x64-Debug/tests/lr1_tests[1]_include.cmake")
+subdirs("../_deps/googletest-build")
