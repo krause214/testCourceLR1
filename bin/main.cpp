725986f5b753b7ff8010a18a67fd85737f213608
Andrey Babincev <andron-2102@mail.ru>
2024-03-07 00:49:36 +0400

    Задал явно массив простых чисел Добавил файл tests.txt в ресурсы Добавил парсер файла tests.txt

--- bin/main.cpp
+++ bin/main.cpp
@@ -6,9 +6,13 @@
 using namespace std;
 
 int main() {
-    vector<int> arr = {1, 1};
-    pair<int, int> result = FirstLabTools::countAndSum(arr);
-    cout << "Number of elements that are squares or cubes of prime numbers: " << result.first << endl;
-    cout << "Sum: " << result.second << endl;
+    vector<vector<int>> arr = FirstLabTools::getNumsFromFile("../resources/tests.txt");
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        cout << "Test number " << i << endl;
+        pair<int, int> result = FirstLabTools::countAndSum(arr[0]);
+        cout << "Number of elements that are squares or cubes of prime numbers: " << result.first << endl;
+        cout << "Sum: " << result.second << endl;
+    }
     return 0;
 }
--- lib/lr1.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <vector>
-
-//std::pair<int, int> countAndSum(const std::vector<int>& array);
